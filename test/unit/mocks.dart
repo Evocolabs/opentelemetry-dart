db196bed7337c313f2481e584d1963f2b52e2bd0
XiaoyuanDu <duxiaoyuan@hiorka.com>
2024-01-31 18:36:30 +0800

    Add mocks for testing loggers

--- test/unit/mocks.dart
+++ test/unit/mocks.dart
@@ -4,7 +4,12 @@
 import 'package:http/http.dart' as http;
 import 'package:mockito/annotations.dart';
 import 'package:opentelemetry/src/api/context/context.dart';
+import 'package:opentelemetry/src/api/logs/log_record.dart';
 import 'package:opentelemetry/src/api/trace/span.dart';
+import 'package:opentelemetry/src/sdk/logs/data/read_write_log_record.dart';
+import 'package:opentelemetry/src/sdk/logs/data/readable_log_record.dart';
+import 'package:opentelemetry/src/sdk/logs/exporters/log_record_exporter.dart';
+import 'package:opentelemetry/src/sdk/logs/log_record_processors/log_record_processor.dart';
 import 'package:opentelemetry/src/sdk/trace/exporters/span_exporter.dart';
 import 'package:opentelemetry/src/sdk/trace/read_only_span.dart';
 import 'package:opentelemetry/src/sdk/trace/span_processors/span_processor.dart';
@@ -15,6 +20,11 @@ import 'package:opentelemetry/src/sdk/trace/span_processors/span_processor.dart'
   MockSpec<Span>(),
   MockSpec<ReadOnlySpan>(),
   MockSpec<SpanExporter>(),
-  MockSpec<SpanProcessor>()
+  MockSpec<SpanProcessor>(),
+  MockSpec<LogRecordProcessor>(),
+  MockSpec<LogRecordExporter>(),
+  MockSpec<LogRecord>(),
+  MockSpec<ReadWriteLogRecord>(),
+  MockSpec<ReadableLogRecord>()
 ])
 export 'mocks.mocks.dart';
